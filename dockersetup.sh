2bb573a2ad6035536c68183ac9335103f2e49c1c
Ubuntu <ubuntu@ip-172-31-36-80.ap-south-2.compute.internal>
2025-07-15 05:25:09 +0000

    DevOps setup files for docker and jenkins

---
+++ dockersetup.sh
@@ -0,0 +1,8 @@
+apt update -y
+apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update -y
+apt install -y docker-ce
+
+exit
---
+++ jenkinssetup.sh
@@ -0,0 +1,11 @@
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+apt update -y
+apt install -y jenkins
+
+exit
